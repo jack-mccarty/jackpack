7129534990ab4bc9d1abbe395f9cc7924570561a
Jack McCarty <jack.mccarty@outlook.com>
2025-04-10 14:05:56 -0500

    deno init

--- jackpack.ts
+++ jackpack.ts
@@ -1,1 +1,12 @@
-console.log('hey hey from JackPack')
+import { serve } from "https://deno.land/std@0.203.0/http/server.ts";
+
+// /home/devops/repositories/jackpack/jackpack.ts
+
+
+const handler = (request: Request): Response => {
+    return new Response("Hello, Deno Deploy!", {
+        headers: { "content-type": "text/plain" },
+    });
+};
+
+serve(handler);
